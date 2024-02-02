5b1c20ff9e3443b5670bf615912384954ce38284
ayesha akhy <akhyemc@gmail.com>
2024-02-02 22:11:45 +0600

    New commit added

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './PRoduct.css';
 
 const Product = (props) => {
-    const {img, name, seller, quantity, price} = props.product;
+    const {img, name, seller, ratings, price} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className="product-info">
+            <h6 className='product-name'>{name}</h6>
+            <p>Price:${price}</p>
+            <p>Manufacturer:{seller}</p>
+            <p>Rating:{ratings} StarsS</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
